7855fbf7b6c079f299d3a07ba11f98222eb7e408
HeineCantor <carmine.palmese@gmail.com>
2023-10-18 23:17:45 +0200

    fix

--- Lempel-Ziv/lempel_ziv.py
+++ Lempel-Ziv/lempel_ziv.py
@@ -1,9 +1,14 @@
 from random import randint
 import math
 import numpy as np
+from tqdm import tqdm
 
 ALPHABET_LENGTH = 4
-SAMPLES_NUMBER = 50
+SAMPLES_NUMBER = 400
+
+SHOW_GENERATED_MESSAGE = True
+SHOW_DICTIONARY = True
+SHOW_ENCODED_MESSAGE = True
 
 def sourceGenerator(alphabetLength, codeLength):
     generatedCode = [str(randint(0, alphabetLength-1)) for i in range(codeLength)]
@@ -14,7 +19,7 @@ def buildLZDictionary(inputCode):
 
     sequenceStart = 0
 
-    for i in range(len(inputCode)):
+    for i in tqdm(range(len(inputCode))):
         potentialPhrase = inputCode[sequenceStart : i + 1]
         if (potentialPhrase not in LZDictionary):
             LZDictionary[potentialPhrase] = len(LZDictionary) + 1
@@ -32,24 +37,42 @@ def LZEncode(dictionary, alphabetLength):
     encodedMessage = ""
     maximumAddressLength = math.floor(math.log2(len(dictionary))) + 1
     maximumValueLength = math.floor(math.log2(alphabetLength))
-    for element in dictionary:
+    for element in tqdm(dictionary):
         pointerToElement = 0
         if(len(element) > 1):
-            pointerToElement = list(dictionary).index(element) + 1
+            if(element[:-1] in dictionary):
+                pointerToElement = list(dictionary).index(element[:-1]) + 1
         encodedMessage += str(bin(pointerToElement))[2:].rjust(maximumAddressLength, "0")
         encodedMessage += str(bin(int(element[-1])))[2:].rjust(maximumValueLength, "0")
 
     return encodedMessage
 
+def LZLemmaValidator(dictionary, samplesNumber):
+    log2n = math.log2(samplesNumber)
+    epsilon = (math.log2(log2n + 2)+2)/log2n
 
+    return len(dictionary) <= samplesNumber/((1-epsilon)*log2n)
+
+print("Generating source message...")
 codeToCompress = sourceGenerator(ALPHABET_LENGTH, SAMPLES_NUMBER)
 
-print(f"SOURCE GENERATED CODE: {codeToCompress}")
+if(SHOW_GENERATED_MESSAGE):
+    print(f"SOURCE GENERATED CODE: {codeToCompress}")
 
+print("Building Lempel-Ziv dictionary...")
 LZDictionary = buildLZDictionary(codeToCompress)
 
-print(f"LEMPEL-ZIV DICTIONARY: {LZDictionary}")
+if(SHOW_DICTIONARY):
+    print(f"LEMPEL-ZIV DICTIONARY: {LZDictionary}")
 
+print("Encoding...")
 encodedMessage = LZEncode(LZDictionary, ALPHABET_LENGTH)
 
-print(f"ENCODED MESSAGE (length={len(encodedMessage)} bits): {encodedMessage}")
+print(f"ENCODED MESSAGE LENGTH: {len(encodedMessage)} bits.")
+
+if(SHOW_ENCODED_MESSAGE):
+    print(f"ENCODED MESSAGE (length={len(encodedMessage)} bits): {encodedMessage}")
+
+print(f"Lempel-Ziv Lemma Validation: {LZLemmaValidator(LZDictionary, SAMPLES_NUMBER)}")
+
+print("Encoding completed.")
